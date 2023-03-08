3452945974d86d0301390b5bb9ae659a71520d15
bigsmart233 <wangyh2@uw.edu>
2023-03-08 11:57:51 -0800

    part

--- app.R
+++ app.R
@@ -1,51 +1,103 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
+library(tidyverse)
+tro <- read.delim("UAH-lower-troposphere-long.csv")
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tabsetPanel(
+    tabPanel("About",
+             "This app uses satellite temperature data from",strong("UAH"),
+             br(),
+             br(),
+             "Temperature", em("temp"),
+             "is measured as deviation (deg C) from 1991-2020 baseline",
+             br(),
+             br(),
+             "The dataset contains 14310 observations and 5 variables. Here is a small (random) sample of data:",
+             mainPanel(tableOutput("firtab"))),
+    tabPanel("Plots",
+             sidebarLayout(
+               sidebarPanel("You can analyze the global temperature for different regions. Select the regions you are interested in. You will see a monthly scatterplot and the corresponding trend lines.",
+                           fluidRow(column(6,
+                                           checkboxInput("show_trend", "Display trend(s)", value = FALSE)
+                                           ),
+                                    column(6,
+                                           uiOutput("checkboxregion") )
+                                    )
+                           ),
+                            
+               mainPanel(plotOutput("plot"))
+             )
+    ),
+    tabPanel("Table",
+             sidebarLayout(
+               sidebarPanel (
+                 "This panel displays average temperature over different time periods: months , years and decades",
+                 radioButtons("time","choose time", choices = c("month","year","decade"))
+               ),
+               mainPanel(
+                 tableOutput("table")
+               )
+             ))
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  # Create reactive data subset based on selected regions
+  sample1 <- reactive({
+    tro %>%
+      filter(region %in% input$region)
+  })
+  
+  output$checkboxregion <- renderUI({
+    checkboxGroupInput("region", "Choose region(s)", choices = unique(tro$region))
+  })
+  
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$firtab <- renderTable({
+    tro %>%
+      sample_n(5)
+  })
+  
+
+  output$plot <- renderPlot({
+    ggplot(sample1(), aes(year, temp)) +
+      geom_point() +
+      if (input$show_trend == TRUE) {
+        geom_smooth(method = "lm", se = FALSE)
+      }
+  })
+  
+  output$table <- renderDataTable({
+    
+    if (input$average_over == "year") {
+      temp_data <- tro %>%
+        group_by(year) %>%
+        summarize(temp = mean(temp))
+    } else if (input$average_over == "decade") {
+      temp_data <- tro %>%
+        group_by(decade) %>%
+        summarize(temp = mean(temp))
+    } else {
+      temp_data <- tro %>%
+        group_by(month) %>%
+        summarize(temp = mean(temp))
+    }
+    
+   
+    temp_data <- temp_data %>%
+      filter(temp >= -0.356153846153846 & temp <= 0.216216216216216)
+    
+    
+    temp_data$decade <- ifelse(
+      input$average_over == "decade",
+      format(temp_data$decade, scientific = FALSE, nsmall = 2),
+     
+    )
+    
+    
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
